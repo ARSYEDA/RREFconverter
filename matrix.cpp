4774ad1673b774c956a5bbc11820e059a3f7b5aa
aryk2 <aryk2@pdx.edu>
2019-04-23 11:15:46 -0700

    made matrix input better

--- matrix.cpp
+++ matrix.cpp
@@ -8,10 +8,13 @@ float ** user_input_matrix(int & rows, int & columns);
 void test_display(float ** matrix, int rows, int columns);
 void free_mem(float ** matrix, int rows, int columns);
 
-//main calls the test function and the 
+//main calls the user input function, displays the input matrix, calls
+//the rref function and then displays the result 
 int main() {
     int rows, columns;
     float ** new_matrix = user_input_matrix(rows, columns);
+    if(new_matrix)
+	test_display(new_matrix, rows, columns);
     float ** returned_matrix = rref_converter(new_matrix, rows, columns);
     if(!returned_matrix)
 	cout << "ERROR: NOT IN REF FORM\n";
@@ -24,6 +27,7 @@ int main() {
     return 0;
 }
 
+//function that allows user to enter a matrix of any dimension
 float ** user_input_matrix(int & rows, int & columns) {
     cout << "\nthis function tests the REF converter"
          << "\nplease enter how many rows are in the system: ";
@@ -32,14 +36,27 @@ float ** user_input_matrix(int & rows, int & columns) {
     cout << "\nplease enter how many columns are in the system: ";
     columns = 0;
     cin >> columns;
+    
+    char col = 'a';
+    cout << "\n\n";
+    for(int k = 0; k < rows; ++k) 
+	cout << "   " << k + 1;
+    cout << endl;
+    for(int i = 0; i < rows; ++i) {
+	cout << char(col+i) << " ";
+	for(int k = 0; k < columns; ++k) 
+	    cout << "[" << char(col+i) << k+1 << "]";
+	cout << endl;
+    }
   
     float** matrix = new float*[rows];
     for(int i = 0; i < rows; ++i)
         matrix[i] = new float[columns];
 
+    cout << "\n";
     for(int i = 0; i < rows; ++i) {
-        cout << "\nrow " << i+1 << ": \n";
         for(int k = 0; k < columns; ++k) {
+	    cout << char(col+i) << k+1 << ":";
             cin >> matrix[i][k];
         }  
         cout << endl;
@@ -47,16 +64,16 @@ float ** user_input_matrix(int & rows, int & columns) {
     return matrix;
 }
 
+//displays matrix passed as arg
 void test_display(float ** matrix, int rows, int columns) {
     for(int i = 0; i < rows; ++i) {
-        cout << endl;
-        for(int k = 0; k < columns; ++k) {
-            cout << matrix[i][k] << "   ";
-        }
+	for(int k = 0; k < columns; ++k) 
+	    cout << "[" << matrix[i][k] << "]";
+	cout << endl;
     }
-    cout << endl;
 }
 
+//frees dynamic memory created when new matrix is made
 void free_mem(float ** matrix, int rows, int columns) {
     if(matrix) {
 	for(int i = 0; i < rows; ++i) {
